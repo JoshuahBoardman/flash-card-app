ffcf2b2ab62a24ede70abac949ba9353e1d80423
JoshuahBoardman <joshuahboardman@gmail.com>
2022-05-23 19:12:51 -0500

    Actually fixed an issue with local storage.

--- src/context/CardCollectionContext.jsx
+++ src/context/CardCollectionContext.jsx
@@ -3,33 +3,28 @@ import React, { useState, useContext, useEffect } from "react";
 const cardCollectionContext = React.createContext();
 
 export function useCardCollection() {
-    return useContext(cardCollectionContext);
+  return useContext(cardCollectionContext);
 }
 
 //TODO: Maybe add random ID generation.
 const CardCollectionContext = ({ children }) => {
-  const [cardCollection, setCardCollection] = useState([]);
-  const [dataLoaded, setDataLoaded] = useState(false)
-
-  useEffect(() => {
+  const [cardCollection, setCardCollection] = useState(() => {
     const data = localStorage.getItem("flashcard-collections");
     if (data) {
-      setCardCollection(JSON.parse(data));
-      setDataLoaded(true);
+      return JSON.parse(data);
     }
-  }, []);
+  });
 
   useEffect(() => {
-    if (!dataLoaded) return
     localStorage.setItem(
       "flashcard-collections",
       JSON.stringify(cardCollection)
     );
-  }, [cardCollection, dataLoaded]);
+  }, [cardCollection]);
 
   return (
     <cardCollectionContext.Provider value={[cardCollection, setCardCollection]}>
-        {children}
+      {children}
     </cardCollectionContext.Provider>
   );
 };
